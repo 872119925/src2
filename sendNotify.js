3fbaee8cfa3539ee72e5af6d9575643b1b23c247
waxx <60652127+872119925@users.noreply.github.com>
2020-11-07 14:41:26 +0800

    Update sendNotify.js

--- sendNotify.js
+++ sendNotify.js
@@ -3,7 +3,7 @@ const $ = new Env();
 // =======================================微信server酱通知设置区域===========================================
 //此处填你申请的SCKEY.
 //注：此处设置github action用户填写到Settings-Secrets里面(Name输入PUSH_KEY)
-let SCKEY = '';
+let SCKEY = 'SCU117715Te7457f3e379aa320fd51f64f5789ad5f5f82e299a4800';
 
 // =======================================Bark App通知设置区域===========================================
 //此处填你BarkAPP的信息(IP/设备码，例如：https://api.day.app/XXXXXXXX)
